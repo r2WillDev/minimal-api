c3441632669b9b92ee3a9d775277812ee0bb5689
r2WillDev <arthurwillyams13@gmail.com>
2025-08-19 21:45:57 -0300

    Validando administrador com login e senha no banco de dados

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
+using MinimalApi.Infraestutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
